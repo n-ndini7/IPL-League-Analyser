5bf7cfd8faffd8657d5a8821850b6aa338257242
n-ndini7 <hp@RJs-PC>
2020-10-28 14:10:17 +0530

    [Nandini] Added UC1 - top batting average

--- src/test/java/com/iplleagueanalyser/IPLLeagueAnalyserTest.java
+++ src/test/java/com/iplleagueanalyser/IPLLeagueAnalyserTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.iplleagueanalyser.SortByChoice.Choice;
+
 public class IPLLeagueAnalyserTest {
 
 	public String BOWLERS_CSV_FILE = "./FactsheetMostWkts.csv";
@@ -12,11 +14,13 @@ public class IPLLeagueAnalyserTest {
 	public List<IPLLeagueBatsmen> batsmen = null;
 	public List<IPLLeagueBowlers> bowlers = null;
 	public IPLLeagueAnalyserExecuter obj;
+	public SortByChoice c1;
 	public List<IPLLeagueBatsmen> l1 = null;
 	public List<IPLLeagueBowlers> l2 = null;
 
 	public IPLLeagueAnalyserTest() {
 		obj = new IPLLeagueAnalyserExecuter();
+		c1 = new SortByChoice();
 	}
 
 	@Test
@@ -31,5 +35,18 @@ public class IPLLeagueAnalyserTest {
 		}
 
 	}
+	// this test case checks for no. of entires in a csv file
+
+	@Test
+	public void givenBattingCSVfileReturnTopAverageBatsmen() {
+		try {
+			batsmen = obj.readData(BATSMEN_CSV_FILE, "BATSMEN");
+		} catch (IPLLeagueAnalyserException e) {
+			e.printStackTrace();
+		}
+		l1 = c1.sortBychoice(Choice.BAT_AVG, batsmen);
+		Assert.assertEquals("83.2", l1.get(0).avg);
+	}
+	// this test case checks for top batting average batsman
 
 }
